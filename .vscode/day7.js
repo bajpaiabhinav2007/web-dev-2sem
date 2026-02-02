05ae712d291b6a531e80d43d492fe2b05888a7bd
bajpaiabhinav2007 <you@example.com>
2026-02-02 15:53:21 +0530

    yes

--- .vscode/day7.js
+++ .vscode/day7.js
@@ -5,3 +5,13 @@ heading.style.backgroundColor = "yellow";
 
 const para = document.getElementsByClassName("my para");
 para.style = backgroundColor = "cyan"
+console.log(para);
+
+for(let i = 0; i < para.length; i++){
+    para[i].style.backgroundColor = "cyan"
+}
+
+
+const qs = document.querySelector("#content p ");
+console.log(qs);
+qs.textContent = "lorem ipsum dolor sit amet, consectetur adipisicing elit. Quisquam, quod."
