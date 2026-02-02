abe419ecaf8571eb38230002751b59ecd1c7dc40
bajpaiabhinav2007 <you@example.com>
2026-02-02 15:40:55 +0530

    yes

--- .vscode/day7.js
+++ .vscode/day7.js
@@ -2,3 +2,6 @@ const heading = document.getElementById("my heading")
 console.log(heading);
 heading.style.color = "tomato";
 heading.style.backgroundColor = "yellow";
+
+const para = document.getElementsByClassName("my para");
+para.style = backgroundColor = "cyan"
